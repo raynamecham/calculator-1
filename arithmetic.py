dff0b1bdf98a06449397cbcddca461b6653abecd
Rayna <rayna.mecham@gmail.com>
2021-01-18 17:02:03 +0000

    Completed math functions

--- arithmetic.py
+++ arithmetic.py
@@ -4,35 +4,40 @@
 def add(num1, num2):
     """Return the sum of the two inputs."""
     sum = num1 + num2
-
     return sum
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
     difference = num1 - num2
-
     return difference
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-
+    product = num1 * num2
+    return product
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
-
+    quotient = num1 / num2
+    return quotient
 
 def square(num1):
     """Return the square of the input."""
-
+    square_result = num1 ** 2
+    return square_result
 
 def cube(num1):
     """Return the cube of the input."""
-
+    cube_result = num1 ** 3
+    return cube_result
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
+    power_result = num1 ** num2
+    return power_result
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    mod_result = num1 % num2
+    return mod_result
